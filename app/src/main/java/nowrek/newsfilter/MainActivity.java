6b0c50f3ab80c1cf7c8e7448d6e70b40f0a37508
rukeros <ruker@protonmail.com>
2017-12-06 15:40:30 +0100

    Added jsoup-1.11.2.jar library to the project.

--- app/src/main/java/nowrek/newsfilter/MainActivity.java
+++ app/src/main/java/nowrek/newsfilter/MainActivity.java
@@ -3,7 +3,6 @@ package nowrek.newsfilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import nowrek.newsfilter.Utils.CppLibrariesLoader;
 
 public class MainActivity extends AppCompatActivity {
