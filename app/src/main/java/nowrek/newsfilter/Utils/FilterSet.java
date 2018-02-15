1bca993088d8e48158bcc4e9af6f34434ebf7732
jedrzej.nowak <jedrzej.nowak.tch@gmail.com>
2018-02-15 15:47:26 +0100

    Adding the articles to process queue

---
+++ app/src/main/java/nowrek/newsfilter/Utils/FilterSet.java
@@ -0,0 +1,14 @@
+package nowrek.newsfilter.Utils;
+
+import nowrek.newsfilter.DataStructures.Article;
+
+/**
+ * Created by jedrzej.nowak on 2018-02-15.
+ */
+
+public class FilterSet {
+    public boolean filterArticle(Article inArticle){
+        //TODO Actual filtering
+        return (inArticle != null);
+    }
+}
